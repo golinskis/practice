a61078cbeb63dc12594a932f755da58c56581520
golinskis <kiszkapolska1>
2018-06-04 11:59:47 +0200

    Creating snipets with html from index.php

--- resources/config.php
+++ resources/config.php
@@ -1,4 +1,8 @@
 <?php
+ob_start();
+session_start();
+
+
 
 defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
 
@@ -12,7 +16,10 @@ defined("DB_USER") ? null : define("DB_USER", "root");
 defined("DB_PASS") ? null : define("DB_PASS", "");
 defined("DB_NAME") ? null : define("DB_NAME", "ecom_db");
 
+$connection = mysqli_connect(DB_HOST,DB_SUER,DB_PASS,DB_NAME);
 
+
+require_once ('functions.php');
 
 
 
