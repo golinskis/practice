4de8eef8b4cae13fa27d78c2c41202e9430fb6ce
golinskis <kiszkapolska1>
2018-06-04 11:23:56 +0200

    Adding path to Database in config.php

--- resources/config.php
+++ resources/config.php
@@ -3,7 +3,28 @@
 defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
 
 
-echo __DIR__;
+defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "templates/front");
+defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "templates/back");
+
+
+defined("DB_HOST") ? null : define("DB_HOST", "localhost");
+defined("DB_USER") ? null : define("DB_USER", "root");
+defined("DB_PASS") ? null : define("DB_PASS", "");
+defined("DB_NAME") ? null : define("DB_NAME", "ecom_db");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
 
