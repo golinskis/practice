e5c2c9d2adc7c5090ef98f5ee0cac84d0e7f0fc1
golinskis <kiszkapolska1>
2018-06-09 01:03:49 +0200

    Seting up class Database

---
+++ admin/includes/Database.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: szymongolinski
+ * Date: 08.06.2018
+ * Time: 23:42
+ */
+require_once ("new_config.php");
+
+class Database
+{
+    public $connection; // tworzenie prywatnej własnoci
+
+    function __construct() //
+    {
+        $this->openDbConnection();
+    }
+
+    public function openDbConnection()//metoda otwierajaca polaczeni za pomoca zincludowanego config.php
+    {
+
+
+//        $this->connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+        $this->connection = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+        if($this->connection->connect_errno){
+            die("Database connection failed badly" . $this->connection->connect_error());
+        }
+    }
+
+
+
+    public function query($sql)
+    {
+        $result = $this->connection->query($sql);
+
+        return $result;
+    }
+
+
+    private function confirmQuery($result)
+    {
+        if(!$result){
+            die("Query Failed". $this->connection->error);
+        }
+
+    }
+
+    public function escapeString($string)
+    {
+       $escapedString =  $this->connection->real_escape_string($string);
+       return $escapedString;
+    }
+
+    public function theInsertID(){
+        return $this->connection->insert_id;
+    }
+
+}
+
+$database = new Database(); // nowy obiekt db
+
--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -7,6 +7,31 @@
                Admin
                 <small>Subheading</small>
             </h1>
+
+
+
+
+
+            <?php
+
+
+
+            $sql = "SELECT * FROM users WHERE id=1";
+
+            $result = $database->query($sql);
+            $userFound = mysqli_fetch_array($result);
+            echo $userFound['username'];
+
+
+
+
+            ?>
+
+
+
+
+
+
             <ol class="breadcrumb">
                 <li>
                     <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
